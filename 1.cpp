b003ec06783e11b550d2a7114452a536f025ded2
u3560670 <47208675+u3560670@users.noreply.github.com>
2019-04-27 22:19:42 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <ctime>
 using namespace std;
 
 const int MAX = 20;
@@ -7,6 +9,7 @@ const int MAX = 20;
 struct Info {
   int size;
   int state;
+  time_t t;
 };
 
 void createTable(Info table[]) {
@@ -29,12 +32,23 @@ void createTable(Info table[]) {
 void showTable(Info table[]) {
   for (int i = 0; i < MAX; i++) {
     if (table[i].size != 0) {
-      cout << "Table " << i+1 << ": ";
-      cout << "size: " << table[i].size;
-      if (table[i].state == 0)
-        cout << "state: Available" << endl;
-      else
-        cout << "state: Not available" << endl;
+      cout << endl << "Table " << i+1 << ": " << endl;
+      cout << "Size: " << table[i].size;
+      if (table[i].state == 0) {
+        cout << "  Time: 0";
+        cout << "  State: Available" << endl;
+      }
+      else {
+        cout << "  Time: " << time(NULL)-table[i].t << "s";
+        if (table[i].state == 1)
+          cout << "  State: Occupied";
+        else if (table[i].state == 2)
+          cout << "  State: Booked";
+        if ((time(NULL)-table[i].t) > 72 && table[i].state == 1)
+          cout << " (Overtime!)" << endl;
+        else if ((time(NULL)-table[i].t) > 36 && table[i].state == 2)
+          cout << " (Booking Overtime! Please release the booking.)" << endl;
+      }
     }
   }
 }
@@ -46,12 +60,15 @@ void occupyTable(Info table[]) {
   while(true) {
     cout << "Table Number: ";
     cin >> num;
-    if (table[num-1].state != 0)
-      cout << "Failed. Table is not vacant.";
-    else
+    if (table[num-1].size == 0)
+      cout << "Table doesn't exist." << endl;
+    else if (table[num-1].state != 0)
+      cout << "Failed. Table is not vacant." << endl;
+    else {
       table[num-1].state = 1;
-
-    cout << "Continue? (Y/N): ";
+      table[num-1].t = time(NULL);
+    }
+    cout << "Occupy another table? (Y/N): ";
     cin >> choose;
     if (choose == 'N')
       break;
@@ -68,7 +85,7 @@ void releaseTable(Info table[]) {
 
     table[num-1].state = 0;
 
-    cout << "Continue? (Y/N): ";
+    cout << "Release another table? (Y/N): ";
     cin >> choose;
     if (choose == 'N')
       break;
@@ -76,17 +93,77 @@ void releaseTable(Info table[]) {
 }
 
 void findTable(Info table[]) {
-  int num, min = 0, diff = MAX;
+  int num, min = -1, diff = MAX;
   cout << "Number of customers: ";
   cin >> num;
 
   for(int i = 0; i < MAX; i++) {
-    if (table[i].size > num && (table[i].size-num) < diff && table[i].state == 0 && table[i].size != 0){
-      min = i;
-      diff = table[i].size-num;
+    if (table[i].state == 0 && table[i].size != 0){
+      if (table[i].size >= num && (table[i].size-num) < diff) {
+        min = i;
+        diff = table[i].size-num;
+      }
     }
   }
-  cout << "Table: " << min << endl;
+  if (min == -1)
+    cout << "No available table was found." << endl;
+  else
+    cout << "Table " << min+1 << " is availtable."<< endl;
+}
+
+void addBooking(Info table[]) {
+  int num;
+  char choose;
+  string name, phone;
+
+  while(true) {
+    cout << "Please enter the name: ";
+    cin >> name;
+    cout << "Please enter phone number: ";
+    cin >> phone;
+    cout << "Table Number: ";
+    cin >> num;
+
+    if (table[num-1].state != 0)
+      cout << "Failed. Table is not vacant.";
+    else {
+      table[num-1].state = 2;
+      table[num-1].t = time(NULL);
+
+      ofstream fout("Booking.txt");
+      if (!fout) {
+        cout << "Failed to open file.";
+        return;
+      }
+      fout << "Name: " << name << "  Phone No.: " << phone << "  Table No.: " << num;
+      fout.close();
+    }
+    cout << "Add another booking? (Y/N): ";
+    cin >> choose;
+    if (choose == 'N')
+      break;
+  }
+}
+
+void releaseBooking(Info table[]) {
+  int num, choose;
+
+  cout << "1: Cancel Booking" << endl;
+  cout << "2: Customer Arrived" << endl;
+  cout << "Please enter your choice (1/2): ";
+  cin >> choose;
+  cout << "Please enter the booked table number: ";
+  cin >> num;
+  if (table[num-1].state == 2) {
+    if (choose == 1)
+      table[num-1].state = 0;
+    else if (choose == 2) {
+      table[num-1].state = 1;
+      table[num-1].t = time(NULL);
+    }
+  }
+  else
+    cout << "Table was not booked." << endl;
 }
 
 int main() {
@@ -95,12 +172,16 @@ int main() {
 
   createTable(table);
   while(true) {
-    cout << "Table Management System" << endl;
+    cout << endl << "Table Management System" << endl;
     cout << "************************" << endl;
     cout << "1: Check Table" << endl;
     cout << "2: Occupy Table" << endl;
     cout << "3: Release Table" << endl;
     cout << "4: Find Table" << endl;
+    cout << "5: Add Booking" << endl;
+    cout << "6: Release Booking" << endl;
+    cout << "7: Reset Tables" << endl;
+    cout << "Please enter your choice (1-7): ";
     cin >> choose;
     for (int i = 0; i < 30; i++) {
       cout << endl;
@@ -117,6 +198,15 @@ int main() {
         break;
       case 4:
         findTable(table);
+        break;
+      case 5:
+        addBooking(table);
+        break;
+      case 6:
+        releaseBooking(table);
+        break;
+      case 7:
+        createTable(table);
         break;
     }
   }
